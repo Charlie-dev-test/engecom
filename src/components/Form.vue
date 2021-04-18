acb46d904badf832ff67782b8cb6b33c61ca1feb
Charlie-dev-test <mailbox.boyko@gmail.com>
2021-04-18 22:09:50 +0300

    feat: form style

---
+++ src/components/Form.vue
@@ -0,0 +1,22 @@
+<template>
+    <form class="engecom-form" action="#">
+        <input class="form-control" type="text" placeholder="Имя">
+        <input class="form-control" type="text" placeholder="Телефон">
+        <input class="form-control" type="text" placeholder="E-mail">
+        <textarea class="form-control" name="question" id="" placeholder="Задайте Ваш вопрос или добавьте комментарий"></textarea>
+        <div class="agreement">
+            <p>Отправляя форму обратной связи, вы <a href="">соглашаетесь</a> с условиями <a href="">обработки и использования персональных данных</a></p>
+        </div>
+        <button class="form-btn" type="submit">ОТПРАВИТЬ</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "Form"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/SectionConnect.vue
+++ src/components/SectionConnect.vue
@@ -1,21 +1,14 @@
 <template>
     <section class="section-connect">
         <div class="sliced-connect">
-            <div class="sliced-connect__bg"></div>
+            <div class="sliced-connect__bg">
+                <div class="yellow"></div>
+            </div>
             <div class="sliced-connect__form">
                 <div class="form-wrap">
                     <h2>СВЯЖИТЕСЬ С НАМИ</h2>
                     <p>и мы ответим на ваши вопросы</p>
-                    <form action="#">
-                        <input class="form-control" type="text" placeholder="Имя">
-                        <input class="form-control" type="text" placeholder="Телефон">
-                        <input class="form-control" type="text" placeholder="E-mail">
-                        <textarea class="form-control" name="question" id="" placeholder="Задайте Ваш вопрос или добавьте комментарий"></textarea>
-                        <div class="agreement">
-                            <p>Отправляя форму обратной связи, вы <a href="">соглашаетесь</a> с условиями <a href="">обработки и использования персональных данных</a></p>
-                        </div>
-                        <button class="form-btn" type="submit">ОТПРАВИТЬ</button>
-                    </form>
+                    <Form/>
                 </div>
             </div>
         </div>
@@ -23,8 +16,12 @@
 </template>
 
 <script>
+    import Form from './Form'
     export default {
-        name: "SectionConnect"
+        name: "SectionConnect",
+        components: {
+            Form
+        }
     }
 </script>
 
